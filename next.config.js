8ead4b16da3bf71b26d763e3c37678872a929380
Jonathan Gan <jon@cryptocurrensea.io>
2022-04-12 20:07:32 -0400

    Testing host on GHpages

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/jongan69/jongan69.github.io/gh-pages/' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/jongan69/jongan69.github.io/FSnextApp/' : '',
 }
